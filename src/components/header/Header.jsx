052210009ff775cea79ae19d91b482e4031611b0
chaymamehrzi <chayma.mehrzi@esprit.tn>
2023-06-03 23:57:48 +0100

    updates

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.jpg'
+import HeaderSocial from './HeaderSocials'
+const Header = () => {
+  return (
+  <header>
+   <div className= "container header__container" >
+       <h5>Hello,I am </h5>
+       <h1>Chayma Mehrzi</h1>
+       <h5 className="text-light">Software Engineering Student</h5>
+       <CTA/>
+       <HeaderSocial/>
+       <div className="me"> 
+      <img src={ME} alt="me"/>
+       </div>
+       <a href="#contact" className='scroll_down'> Scroll Down</a>
+   </div>
+  </header>
+  )
+}
+
+export default Header
