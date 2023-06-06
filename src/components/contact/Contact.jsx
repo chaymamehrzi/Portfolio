2ab25b2e75b5a7a9d760d4d688316e34a583fb21
chaymamehrzi <chayma.mehrzi@esprit.tn>
2023-06-06 18:39:36 +0100

    updates

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,19 @@ import './contact.css'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+     <h5>Get In Touch</h5>
+     <h2>Contact Me</h2>
+     <div className="container contact__container">
+      <div className="contact__options">
+      </div>
+      {/* End of contact options */}
+        <form action="">
+
+        </form>
+      
+     </div>
+    </section>
   )
 }
 
